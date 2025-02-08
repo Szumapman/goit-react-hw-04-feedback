94072028dc8309ab5d6e95790a21a816ac58baab
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-08 16:00:34 +0100

    change repo name for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/goit-react-hw-02-feedback/',
+  base: '/goit-react-hw-04-feedback/',
   plugins: [react()],
 })
