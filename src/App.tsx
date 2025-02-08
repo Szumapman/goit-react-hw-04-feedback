b514064ffbc2f24253920ae6279bfaffd0cd062b
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-08 15:57:48 +0100

    change total and positivePercentage from variable to state and add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import { FeedbackHeader } from './components/FeedbackHeader';
 import { FeedbackOptions } from './components/FeedbackOptions';
 import { STAT_OPTIONS_ARRAY } from './constants/StatOptions.ts';
-import { INITIAL_FEEDBACK_STATE } from './constants/InitialFeedbackState.ts';
+import { INITIAL_FEEDBACK_STATE } from "./constants/InitialFeedbackState.ts";
 import { Feedback } from "./constants/Feedback";
 import { FeedbackStat } from "./components/FeedbackStat";
 import { NoFeedbackNotification } from "./components/NoFeedbackNotification";
 
 
+
 function App() {
   const [feedback, setFeedback] = useState(INITIAL_FEEDBACK_STATE);
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
   
   const onFeedback = (e: { target: { innerText: string; }; }) => {
     setFeedback((prev: Feedback) => ({
@@ -19,8 +22,11 @@ function App() {
     }))
   };
 
-  const total = feedback.good + feedback.neutral + feedback.bad;
-  const positivePercentage = total > 0 ? Math.round((feedback.good / total) * 100) : 0;
+  useEffect(() => {
+    setTotal(feedback.good + feedback.neutral + feedback.bad);
+    setPositivePercentage(Math.round((feedback.good / (feedback.good + feedback.neutral + feedback.bad)) * 100));
+  }, [feedback]);
+
 
   return (
     <>
